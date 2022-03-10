b99d6629f473fd14249086b6ed841c034d8bf5cb
MesutRuzgar <mesutruzgar@gmail.com>
2022-03-10 18:52:45 +0300

    string methods example

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -12,6 +12,62 @@ namespace Methods
         {
             //MathMethods();
 
+            string metin = "Korkma, sönmez bu şafaklarda yüzen al sancak";
+            int karakterSayisi = metin.Length;
+            string kopyaMetin = string.Copy(metin);
+
+            bool kelimeVarMi = metin.Contains("korkma"); //true yada false döner. içinde varmı diye aratır
+
+            bool nvileBiterMi = metin.EndsWith(";");
+
+            //bendekiyle gelen eşit mi diye kontrol eder
+            bool esitMi = metin.Equals("İstiklar Marşı");
+
+            int sIndex = metin.IndexOf("s");
+
+            string buyukHarfliMetin = metin.ToUpper();
+            string kucukHarfliMetin = metin.ToLower();
+
+            //insert ile istediğimiz index numarasına göre ekleme yapabiliriz
+            //metin+= "...." diyerek te sonuna ekleyebiliriz istersek
+            string yeniMetin = metin.Insert(metin.Length - 1, "Sönmeden yurdumun üstünde tüten en son ocak.");
+            metin += "O benim milletimin yıldızıdır parlayak;;;";
+            metin = metin.Remove(metin.Length - 2);
+
+            //boşluk olan yeri _ yapıyoruz
+            metin=metin.Replace(" ", "_");
+
+            //metni kelimelere parçalama
+            string[] kelimeler = metin.Split('_');
+            foreach (var item in kelimeler)
+            {
+                Console.WriteLine("parçalanan metin");
+                Console.WriteLine(item);
+            }
+
+            char[] ilkKelimelerinKarakterleri = kelimeler[0].ToCharArray();
+            foreach (var item in ilkKelimelerinKarakterleri)
+            {
+                Console.WriteLine("ilk kelimelerin karakterleri");
+                Console.WriteLine(item);
+            }
+
+            //boşlukları silmemize yarar
+            metin.Trim();
+
+            //split herhangi bir özel karaktere göre sürekli böler
+            //substring ise isteğimiz index arasındaki bilgileri kesip alır
+            string parcalananMetin=metin.Substring(20, 10);
+            Console.WriteLine("substring ile Parçalanan metin:{0}",parcalananMetin);
+
+            Console.WriteLine("Metnin karakter sayısı:{0}",karakterSayisi);
+            Console.WriteLine("kopya metin:{0}", kopyaMetin);
+            Console.WriteLine("korkma kelime varmi:{0}", kelimeVarMi);
+            Console.WriteLine("; ile biter mi:{0}", nvileBiterMi);
+            Console.WriteLine("istiklal marşına eşit mi:{0}", esitMi);
+            Console.WriteLine("S nin indeksi:{0}", sIndex);
+            Console.WriteLine("büyük harfli metin:{0}", buyukHarfliMetin);
+            Console.WriteLine("küçük harfli metin:{0}", kucukHarfliMetin);
 
             Console.ReadLine();
         }
