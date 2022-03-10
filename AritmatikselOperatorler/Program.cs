5966800de4b1cc84c3c25fbf27a7ada01bb892a6
MesutRuzgar <mesutruzgar@gmail.com>
2022-03-10 13:59:20 +0300

    aritmatikselIslemler example

--- AritmatikselOperatorler/Program.cs
+++ AritmatikselOperatorler/Program.cs
@@ -10,6 +10,24 @@ namespace AritmatikselOperatorler
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("1. sayıyı giriniz.");
+            int sayi1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("2. sayıyı giriniz.");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            int toplam = sayi1 + sayi2;
+            Console.WriteLine("Toplama sonucu={0}", toplam);
+            int fark = sayi1 - sayi2;
+            Console.WriteLine("Çıkartma sonucu={0}", fark);
+            int carp = sayi1 * sayi2;
+            Console.WriteLine("Çarpma sonucu={0}", carp);
+            int bolum = sayi1 / sayi2;
+            Console.WriteLine("Bölme sonucu={0}",bolum);
+            //mod almak için => % yani bölümden kalanı bulmak için
+            int mod = sayi1 % sayi2;
+            Console.WriteLine("Mod yani bölümden kalan={0}",mod);
+
+            Console.ReadLine();
         }
     }
 }
