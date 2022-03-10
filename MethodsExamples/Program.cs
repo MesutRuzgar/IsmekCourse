6d62fdc32fc6e16840ed47f82441c61689a51892
MesutRuzgar <mesutruzgar@gmail.com>
2022-03-10 20:28:16 +0300

    methods example 1

--- MethodsExamples/Program.cs
+++ MethodsExamples/Program.cs
@@ -10,6 +10,74 @@ namespace MethodsExamples
     {
         static void Main(string[] args)
         {
+            int sayi;
+            //Console.WriteLine("3 haneli bir sayı giriniz.");
+            //sayi = int.Parse(Console.ReadLine());
+            //if (sayi<100 || sayi>999)
+            //{
+            //    Console.WriteLine("Sayı 3 haneli olmalıdır.");
+            //    Console.ReadLine();
+            //    //return yazarak methoddan cıkarttık yani bitirdik
+            //    return;
+            //}
+
+            char cevap;
+            do
+            {
+                do
+                {
+                    Console.WriteLine("3 haneli bir sayı giriniz.");
+                    sayi = int.Parse(Console.ReadLine());
+
+                } while (sayi < 100 || sayi > 999);
+                Console.WriteLine("Tuttuğun sayıyı tersten düşün.");
+                Console.ReadLine();
+                Console.WriteLine("Büyük olan sayıdan küçük sayıyı çıkar.");
+                Console.ReadLine();
+                Console.WriteLine("Sonucun son rakamı ne?");
+                int sonRakam = int.Parse(Console.ReadLine());
+                switch (sonRakam)
+                {
+                    case 1:
+                        Console.WriteLine("İşleminin sonucu 891.");
+                        break;
+                    case 2:
+                        Console.WriteLine("İşleminin sonucu 122.");
+                        break;
+                    case 3:
+                        Console.WriteLine("İşleminin sonucu 313.");
+                        break;
+                    case 4:
+                        Console.WriteLine("İşleminin sonucu 424.");
+                        break;
+                    case 5:
+                        Console.WriteLine("İşleminin sonucu 495.");
+                        break;
+                    case 6:
+                        Console.WriteLine("İşleminin sonucu 396.");
+                        break;
+                    case 7:
+                        Console.WriteLine("İşleminin sonucu 297.");
+                        break;
+                    case 8:
+                        Console.WriteLine("İşleminin sonucu 198.");
+                        break;
+                    case 9:
+                        Console.WriteLine("İşleminin sonucu 099.");
+                        break;
+                    case 0:
+                        Console.WriteLine("İşleminin sonucu 000.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Son rakamı girdiğinizden emin olunuz.");
+                        break;
+                }
+                Console.WriteLine("Devam etmek istiyor musunuz?(e/h)");
+                cevap = Convert.ToChar(Console.ReadLine());
+
+            } while (cevap=='e');
+            Console.ReadLine();
         }
     }
 }
