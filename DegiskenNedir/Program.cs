76cc05197add1920896e44c3f0e9bf915c22cd3a
MesutRuzgar <mesutruzgar@gmail.com>
2022-03-10 13:51:37 +0300

    Add project files.

---
+++ DegiskenNedir/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegiskenNedir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string isim = "Mesut";
+            int tamSayi = 10;
+            double ondalikli=15.2;
+            DateTime tarih = DateTime.Now;
+            bool cevap = true;
+
+            Console.WriteLine(isim);
+            Console.WriteLine(tamSayi);
+            Console.WriteLine(ondalikli);
+            Console.WriteLine(tarih);
+
+            Console.WriteLine(cevap);
+
+            Console.ReadLine();
+        }
+
+    }
+
+}
---
+++ TurDonusumleri/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurDonusumleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sevgili kursiyer, hangi şehirde yaşıyorsun?");
+            string sehir =Console.ReadLine();
+            //Console.WriteLine(sehir+" şehrinin il plaka kodu nedir?");
+            Console.WriteLine("{0} şehrinin il plaka kodu nedir?",sehir);
+            int plaka=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} şehrinin il plaka kodu= {1}",sehir,plaka);
+            string tarih=Convert.ToString(DateTime.Now);
+            Console.WriteLine(tarih);
+            Console.ReadKey();
+
+        }
+
+    }
+}
