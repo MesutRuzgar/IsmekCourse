bbc0740f58d4b92ad120ea0fa556b7cce252c791
MesutRuzgar <mesutruzgar@gmail.com>
2022-03-10 20:39:07 +0300

    kura programı example

--- MethodsExamples/Program.cs
+++ MethodsExamples/Program.cs
@@ -10,6 +10,31 @@ namespace MethodsExamples
     {
         static void Main(string[] args)
         {
+            //Example1();
+            int secim;
+            do
+            {
+                Console.WriteLine("Kuraya kaç kişi katılacak?");
+                int kisiSayisi = int.Parse(Console.ReadLine());
+                string[] kisiListesi = new string[kisiSayisi];
+                for (int i = 0; i < kisiListesi.Length; i++)
+                {
+                    Console.WriteLine("Kuraya katılacak ismi giriniz.");
+                    kisiListesi[i] = Console.ReadLine();
+                }
+                int kuraSiralamasi = new Random().Next(0, kisiSayisi - 1);
+                Console.WriteLine("Kazanan:{0}", kisiListesi[kuraSiralamasi]);
+                Console.WriteLine("Yeni bir kura başlatmak için 1'i tuşlayınız.");
+                secim = int.Parse(Console.ReadLine());
+                Console.Clear();
+
+            } while (secim==1);
+
+            Console.ReadLine();
+        }
+
+        private static void Example1()
+        {
             int sayi;
             //Console.WriteLine("3 haneli bir sayı giriniz.");
             //sayi = int.Parse(Console.ReadLine());
@@ -76,8 +101,7 @@ namespace MethodsExamples
                 Console.WriteLine("Devam etmek istiyor musunuz?(e/h)");
                 cevap = Convert.ToChar(Console.ReadLine());
 
-            } while (cevap=='e');
-            Console.ReadLine();
+            } while (cevap == 'e');
         }
     }
 }
